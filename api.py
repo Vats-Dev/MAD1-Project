fd2a2728608d3d403f6df8695b0bd6a0f8e0ab4a
Dev Vats <devvats.pai@gmail.com>
2024-04-07 14:47:54 +0530

    Added API and styling

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource, Api
+from app import app
+from models import db, Section
+
+api = Api(app)
+
+class SectionResource(Resource):
+    def get(self):
+        sections = Section.query.all()
+        return {'sections': [ {
+            'id': section.id,
+            'name': section.name
+        } for section in sections]
+        }
+
+
+api.add_resource(SectionResource, '/api/section')
